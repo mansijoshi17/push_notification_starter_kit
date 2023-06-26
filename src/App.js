02b06dce676d3a2ba8c64af5319042434721b885
mansijoshi17 <mansijoshi1777@gmail.com>
2023-06-26 12:56:34 +0530

    Added dotenv

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import PrimarySearchAppBar from "./components/AppBar";
 import SendNotification from "./components/SendNotifications";
+import process from "process";
+import dotenv from "dotenv";
 
 function App() {
+  useEffect(() => {
+    dotenv.config();
+  }, []);
+
   return (
     <div className="App">
       <PrimarySearchAppBar />
